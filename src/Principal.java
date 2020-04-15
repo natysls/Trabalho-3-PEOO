6089d72f5595fa13daa6004e300001be427eca69
Natália <Natlia@192.168.0.22>
2020-04-15 16:55:12 -0300

    tentativa de identação

--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Principal {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		ArrayList<Canal> canais = new ArrayList<Canal>();
 		canais.add(new Canal(2, "RedeTV", false));
 		canais.add(new Canal(8, "Record", false));
@@ -9,79 +9,79 @@ public class Principal {
 		canais.add(new Canal(12, "SBT", true));
 		canais.add(new Canal(19, "RedeBrasil", false));
 		canais.add(new Canal(20, "Band", true));
-		
-		Televisao smarttv = new SmartTV(canais, 10); //polegadas
-		Televisao tvhd = new TVHD(canais, "LED"); //modelo
-		
-		smarttv.cadastrarCanais(); 
-		tvhd.cadastrarCanais(); //vai cadastrar so quem é HD
-		
+
+		Televisao smarttv = new SmartTV(canais, 10); // polegadas
+		Televisao tvhd = new TVHD(canais, "LED"); // modelo
+
+		smarttv.cadastrarCanais();
+		tvhd.cadastrarCanais(); // vai cadastrar so quem é HD
+
 		ControleRemoto controle = new ControleRemoto();
 		try {
 			controle.adicionarTV(smarttv);
 			controle.adicionarTV(tvhd);
-		}catch(ExceptionTVCadastrada e) {
+		} catch (ExceptionTVCadastrada e) {
 			System.out.println(e);
 		}
-		
+
 		Scanner teclado = new Scanner(System.in);
-		while(true) {
+		while (true) {
 			System.out.println("Escolha a sua TV: ");
 			System.out.println("A - SmartTV  ou  B - TVHD");
 			String escolha = teclado.next();
-			if(escolha.equalsIgnoreCase("A")) {
+			if (escolha.equalsIgnoreCase("A")) {
 				opcoes(smarttv, controle);
-			}else if(escolha.equalsIgnoreCase("B")) {
+			} else if (escolha.equalsIgnoreCase("B")) {
 				opcoes(tvhd, controle);
-			}else {
+			} else {
 				System.out.println("Opção inválida");
 			}
 		}
 
-		
-		
 	}
-	
-	
+
 	private static void opcoes(Televisao tv, ControleRemoto controle) {
 		Scanner scan = new Scanner(System.in);
-    	while (true) { //loop infinito.
-    		printMenu(); //imprime o Menu
-    		int opcao = scan.nextInt();
+		while (true) { // loop infinito.
+			printMenu(); // imprime o Menu
+			int opcao = scan.nextInt();
 			if (opcao == 1) {
-    			controle.aumentarVolume();
-    			System.out.println("Volume: " + tv.getVolume() + "\n");
-    		} else if (opcao == 2) {
+				controle.aumentarVolume();
+				System.out.println("Volume: " + tv.getVolume() + "\n");
+			} else if (opcao == 2) {
 				controle.diminuirVolume();
 				System.out.println("Volume: " + tv.getVolume() + "\n");
 			} else if (opcao == 3) {
 				controle.proximoCanal();
-    			System.out.println("Canal: " + tv.getCanalAtual() + "\n");
+				System.out.println("Canal: " + tv.getCanalAtual() + "\n");
 			} else if (opcao == 4) {
 				controle.anteriorCanal();
-    			System.out.println("Canal: " + tv.getCanalAtual() + "\n");
-			}else if (opcao == 5) {
+				System.out.println("Canal: " + tv.getCanalAtual() + "\n");
+			} else if (opcao == 5) {
 				controle.informarDados();
-			}else if (opcao == 6) {
-				//controle.sintonizarCanal(numCanal);
+			} else if (opcao == 6) {
+				// controle.sintonizarCanal(numCanal);
+			} else if (opcao == 7) {
+				controle.mostrarGrade();
 			} else if (opcao == 0) { // o Loop para aqui.
 				System.exit(0); // Saindo do programa
 			} else {
 				System.out.println("Opção inválida");
 				printMenu();
 			}
-    	}
-	}
-	
-	//Imprime o menu
-		private static void printMenu() {
-			System.out.println("Digite a opção desejada:");
-			System.out.println("1 - Aumentar Volume");
-			System.out.println("2 - Diminuir Volume");
-			System.out.println("3 - Proximo Canal");
-			System.out.println("4 - Anterior Canal");
-			System.out.println("5 - Informar Dados do Canal");
-			System.out.println("6 - Sintonizar Canal");
-			System.out.println("0 - EXIT");
 		}
+	}
+
+	// Imprime o menu
+	private static void printMenu() {
+		System.out.println("Digite a opção desejada:");
+		System.out.println("1 - Aumentar Volume");
+		System.out.println("2 - Diminuir Volume");
+		System.out.println("3 - Proximo Canal");
+		System.out.println("4 - Anterior Canal");
+		System.out.println("5 - Informar Dados do Canal");
+		System.out.println("6 - Sintonizar Canal");
+		System.out.println("7 - Mostrar Grade");
+		System.out.println("0 - EXIT");
+	}
 }
--- src/TVHD.java
+++ src/TVHD.java
@@ -1,31 +1,30 @@
 import java.util.ArrayList;
 
-public class TVHD extends Televisao{
-	private String modelo; //LED, PLASMA, HD
-	
+public class TVHD extends Televisao {
+	private String modelo; // LED, PLASMA, HD
+
 	public String getModelo() {
 		return modelo;
 	}
+
 	public void setModelo(String modelo) {
 		this.modelo = modelo;
 	}
 
-	public TVHD(ArrayList<Canal> canaisDisponiveis, String modelo){
+	public TVHD(ArrayList<Canal> canaisDisponiveis, String modelo) {
 		super(canaisDisponiveis);
 		this.modelo = modelo;
 	}
 
 	@Override
 	public void cadastrarCanais() {
-		for(Canal canal: canaisDisponiveis) {
-			if(canal.isEhHD() == true) {
+		for (Canal canal : canaisDisponiveis) {
+			if (canal.isEhHD() == true) {
 				this.canaisCadastrados.add(canal);
-			}else {
-				System.out.println("Canal não cadastrado pois não é HD");
 			}
 		}
-		this.canalAtual = this.canaisCadastrados.get(canaisCadastrados.size() - 1); //ultimo canal
-		
+		this.canalAtual = this.canaisCadastrados.get(canaisCadastrados.size() - 1); // ultimo canal
+
 	}
 
 }
